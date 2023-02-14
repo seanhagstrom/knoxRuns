d50e2bdb1216e762543132da5699dbe025c2e4e7
Sean Hagstrom <seanhagstrom1@gmail.com>
2023-02-14 13:30:00 -0500

    refactor(oauth): move handshake to server side

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -26,34 +26,3 @@ export const loginUser = async ({ username, password }) => {
     console.error(error);
   }
 };
-
-// I may move this to the backend for security purposes.
-export const stravaAuth = async (code) => {
-  console.log({
-    client_id: VITE_STRAVA_CLIENT_ID,
-    client_secret: VITE_STRAVA_CLIENT_SECRET,
-  });
-  try {
-    const oauthURL = `https://www.strava.com/api/v3/oauth/token`;
-
-    const response = await fetch(oauthURL, {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json, text/plain, */*',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        client_id: VITE_STRAVA_CLIENT_ID,
-        client_secret: VITE_STRAVA_CLIENT_SECRET,
-        code,
-        grant_type: 'authorization_code',
-      }),
-    });
-
-    const data = await response.json();
-    console.log(data);
-    return data;
-  } catch (error) {
-    console.error(error);
-  }
-};
--- server/auth/index.js
+++ server/auth/index.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
+const axios = require('axios');
+const { VITE_STRAVA_CLIENT_ID, VITE_STRAVA_CLIENT_SECRET } = process.env;
 const { authenticateUser } = require('../db');
+const BASE_URL = `http://localhost:5173`;
 
 // GET auth
 router.get('/', (req, res, next) => {
@@ -8,6 +11,44 @@ router.get('/', (req, res, next) => {
   } catch (error) {
     console.error(error);
     throw error;
+  }
+});
+
+// GET auth/exchange_token
+router.get('/exchange_token', async (req, res, next) => {
+  const {
+    query: { code },
+  } = req;
+  try {
+    console.log(code);
+
+    // const header = {
+    //   headers: {
+    //     Accept: 'application/json, text/plain, */*',
+    //     'Content-Type': 'application/json',
+    //   },
+    // };
+
+    const { data } = await axios.post(
+      `https://www.strava.com/oauth/token`,
+      {
+        client_id: VITE_STRAVA_CLIENT_ID,
+        client_secret: VITE_STRAVA_CLIENT_SECRET,
+        code,
+        grant_type: 'authorization_code',
+      },
+      {
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+    console.log(data);
+
+    res.redirect(`${BASE_URL}`);
+  } catch (error) {
+    next(error);
   }
 });
 
